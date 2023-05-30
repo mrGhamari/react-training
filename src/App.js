94ee49aac00b28d68476b349e3466a9e72561720
mmdrza <mmdrza77@gmail.com>
2023-05-30 11:06:14 +0330

    clear expense list array

--- src/App.js
+++ src/App.js
@@ -5,27 +5,8 @@ import NewExpense from "./components/NewExpense/newExpense";
 import ExpenseItem from "./components/ExpenseItem/ExpenseItem";
 
 function App() {
-  const [expenses, setNewExpenses] = useState([
-    {
-      id: "e1",
-      title: "Toilet Paper",
-      amount: 94.12,
-      date: new Date(2020, 7, 14),
-    },
-    { id: "e2", title: "New TV", amount: 799.49, date: new Date(2021, 2, 12) },
-    {
-      id: "e3",
-      title: "Car Insurance",
-      amount: 294.67,
-      date: new Date(2021, 2, 28),
-    },
-    {
-      id: "e4",
-      title: "New Desk (Wooden)",
-      amount: 450,
-      date: new Date(2021, 5, 12),
-    },
-  ]);
+  const expenseArray = [];
+  const [expenses, setNewExpenses] = useState(expenseArray);
   const addExpenseHandler = (expense) => {
     setNewExpenses((prevExpenses) => {
       return [expense, ...prevExpenses];
@@ -35,8 +16,8 @@ function App() {
     <div className="App">
       <NewExpense onAddExpense={addExpenseHandler} />
       <Card className="wrapper">
-        {expenses.map((expense) => (
-          <ExpenseItem data={expense} />
+        {expenses.map((item, index) => (
+          <ExpenseItem key={index} data={item} />
         ))}
       </Card>
     </div>
